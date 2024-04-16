326d4288e48726a477f830f329e3fbd86f4026be
kobashi <hax@tacos.com>
2024-04-16 15:21:29 -0500

    Fambook v1.5

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -8,7 +8,7 @@ const UserImage = ({ image, size = "60px" }) => {
         width={size}
         height={size}
         alt="user"
-        src={`https://fambook.onrender.com/assets/${image}`}
+        src={`https://fambook-1.onrender.com/assets/${image}`}
       />
     </Box>
   );
